01a92889c1ecc27d340b4a1c057fb2468605edfb
SirKotoff <68810705+SirKotoff@users.noreply.github.com>
2021-04-24 22:37:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+using System.Collections;
+
+
+namespace xml_lab_2
+{
+    class Program
+    {
+
+        public static void T1V()
+        {
+            string path = @"C:\Users\Student\Desktop\xml_lab_2\File.xml";
+
+            XDocument xdocument = XDocument.Load(path);
+
+            Console.WriteLine(xdocument.ToString());
+            Console.WriteLine("________");
+            foreach (var element in xdocument.Root.Elements())
+            {
+                var max = (from e in element.Descendants() select e.Attributes().Count()).DefaultIfEmpty(-1).Max();
+
+                var a = (from e in element.Descendants() where e.Attributes().Count() == max let name = e.Name.LocalName orderby name  select name).DefaultIfEmpty("no child");
+
+                foreach (var e in a)
+                {
+                    Console.WriteLine($"{e} [{max}]");
+                }
+
+            }
+
+            Console.WriteLine("________");
+        }
+
+        static void Main(string[] args)
+        {
+            T1V();
+            Console.ReadKey();
+        }
+    }
+}
